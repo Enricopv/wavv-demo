ec844059477429209c6777bb24d3b0bc6702db9d
Enrico Valbuena <enrico@valpunk.com>
2022-02-04 09:35:18 -0700

    error report

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -6,15 +6,23 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 const client = twilio(accountSid, authToken);
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  client.messages
-    .create({
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const message = await client.messages.create({
       body: "bananan",
       from: "+19032963527",
       to: "+18052312053",
-    })
-    .then((message: { sid: string }) => console.log(message.sid));
+    });
 
-  res.writeHead(200, { "Content-Type": "text/xml" });
-  res.json({ message: "done" });
+    console.log(message?.sid);
+
+    res.writeHead(200, { "Content-Type": "text/xml" });
+    res.json({ message: "done" });
+  } catch (e) {
+    res.writeHead(400, { "Content-Type": "text/xml" });
+    res.json({ error: e });
+  }
 }
